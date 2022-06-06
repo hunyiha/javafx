439157a6e5b2e745f73419b7df391b6d35f2d5b6
guangpeng.he <2868266849@qq.com>
2022-06-06 19:35:03 +0800

    Stage窗口类型

--- Lesson4/src/com/hunyiha/javafx/Main.java
+++ Lesson4/src/com/hunyiha/javafx/Main.java
@@ -2,7 +2,7 @@ package com.hunyiha.javafx;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.omg.CORBA.portable.ApplicationException;
+import javafx.stage.StageStyle;
 
 /**
  * @author:hunyiha
@@ -11,10 +11,36 @@ import org.omg.CORBA.portable.ApplicationException;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        /**
+         * 默认Stage是DECORATED
+         */
+        Stage s1 = new Stage();
+        s1.setTitle("s1");
+        s1.initStyle(StageStyle.DECORATED);
+        s1.show();
 
+        /**
+         * 透明的
+         */
+        Stage s2 = new Stage();
+        s2.setTitle("s2");
+        s2.initStyle(StageStyle.TRANSPARENT);
+        s2.show();
+
+        Stage s3 = new Stage();
+        s3.setTitle("s3");
+        s3.show();
+
+        Stage s4 = new Stage();
+        s4.setTitle("s4");
+        s4.show();
+
+        Stage s5 = new Stage();
+        s5.setTitle("s5");
+        s5.show();
     }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
